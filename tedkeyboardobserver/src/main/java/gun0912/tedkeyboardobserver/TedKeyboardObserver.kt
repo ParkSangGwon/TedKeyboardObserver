af9b1bdd6635383ee0dee87ecd33e31db32eaebf
sjjeong <sjjeong1225@gmail.com>
2019-02-27 23:57:08 +0900

    Add a listen function that takes a higher-order function as a parameter

--- tedkeyboardobserver/src/main/java/gun0912/tedkeyboardobserver/TedKeyboardObserver.kt
+++ tedkeyboardobserver/src/main/java/gun0912/tedkeyboardobserver/TedKeyboardObserver.kt
@@ -7,4 +7,12 @@ class TedKeyboardObserver(activity: Activity) : BaseKeyboardObserver(activity) {
     fun listen(onKeyboardListener: OnKeyboardListener) {
         internalListen(onKeyboardListener)
     }
+
+    fun listen(action: (Boolean) -> Unit) {
+        internalListen(object : OnKeyboardListener {
+            override fun onKeyboardChange(isShow: Boolean) {
+                action(isShow)
+            }
+        })
+    }
 }
